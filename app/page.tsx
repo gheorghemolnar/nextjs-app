6fb5e98b912b7b7ac2fe24714a3e89a13f3c5f41
loumnar <molnargheorghe@yahoo.com>
2023-05-25 21:27:28 +0200

    update: IndexPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,19 @@
-import Link from "next/link"
-
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 
-export default function IndexPage() {
+export default async function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
+          Lorem ipsum dolor sit amet consectetur adipisicing natus.
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
+          Projet RTI
         </p>
       </div>
       <div className="flex gap-4">
-        <Link
+        {/*         <Link
           href={siteConfig.links.docs}
           target="_blank"
           rel="noreferrer"
@@ -32,7 +28,7 @@ export default function IndexPage() {
           className={buttonVariants({ variant: "outline", size: "lg" })}
         >
           GitHub
-        </Link>
+        </Link> */}
       </div>
     </section>
   )
