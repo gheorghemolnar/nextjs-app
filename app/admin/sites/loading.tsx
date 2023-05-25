fdd4633b0e2f82cc49841f6ad1586a47db4f1dbc
loumnar <molnargheorghe@yahoo.com>
2023-05-25 21:24:32 +0200

    feat: add SITES loading page

---
+++ app/admin/sites/loading.tsx
@@ -0,0 +1,17 @@
+import { Skeleton } from "@/components/ui/skeleton"
+
+export default function loading() {
+  return (
+    <section className="container grid items-center align-items: center gap-6 pb-8 pt-6 md:py-10">
+      <div className="flex max-w-[980px] flex-col justify-items-center items-center gap-2">
+        <div className="flex gap-4 w-[250px] h-[250px]">
+          <Skeleton className="h-12 w-12 rounded-full" />
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-[250px]" />
+            <Skeleton className="h-4 w-[200px]" />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
