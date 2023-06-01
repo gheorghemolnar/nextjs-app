03434093fceb4ee379abac41a71798c178b768f7
loumnar <molnargheorghe@yahoo.com>
2023-06-01 13:40:44 +0200

    add: utils add formating date, ids

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatSiteId(id: number) {
+  return `${id}`.padStart(2, "0")
+}
+
+export function formatDate(dateInput: string) {
+  const date = new Date(dateInput)
+
+  if (!date) return "--"
+
+  return `${date.toLocaleDateString()} à ${date.toLocaleTimeString()}`
+}
