a0481a25b1cc04ec2c376e124e2b1ae99d37190a
loumnar <molnargheorghe@yahoo.com>
2023-06-01 13:34:35 +0200

    upd: main page

--- app/page.tsx
+++ app/page.tsx
@@ -6,30 +6,13 @@ export default async function IndexPage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing natus.
+          Projet RTI
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
-          Projet RTI
+          Service Informatique - Bigard
         </p>
       </div>
-      <div className="flex gap-4">
-        {/*         <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants({ size: "lg" })}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline", size: "lg" })}
-        >
-          GitHub
-        </Link> */}
-      </div>
+      <div className="flex gap-4">A COMPLETER</div>
     </section>
   )
 }
