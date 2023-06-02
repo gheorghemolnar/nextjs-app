17a5c34eefa4e37edb069d4c7e134c57219ad833
loumnar <molnargheorghe@yahoo.com>
2023-06-02 09:46:40 +0200

    upd: ajout espace au niveau Heading

--- components/ui/listing/listing.tsx
+++ components/ui/listing/listing.tsx
@@ -28,10 +28,12 @@ export default function Listing({ columns, data, getData, siteId }: Props) {
     <>
       <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
         <div className="flex items-center justify-between space-y-2">
+          {/*           
           <div>
             <h2 className="text-2xl font-bold tracking-tight">Contrôles</h2>
             <p className="text-muted-foreground">Liste de résultats</p>
           </div>
+          */}
         </div>
 
         <DataTable
