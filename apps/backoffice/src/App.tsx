3afe76f3941fc5803da8829cb80d1787bfcb93f4
gheorghe <gheorghe@macbook.local>
2023-06-13 22:08:29 +0200

    refactor(backoffice): handle ui and tailwind config packages

--- apps/backoffice/src/App.tsx
+++ apps/backoffice/src/App.tsx
@@ -1,5 +1,12 @@
+import { Bonjour } from '@big/ui';
+
 function App() {
-    return <p className="text-red-500 text-xl text-center">BONJOUR</p>;
+    return (
+        <>
+            <p className="text-green-500 text-xl text-center">BONJOUR</p>
+            <Bonjour />
+        </>
+    );
 }
 
 export default App;
--- apps/backoffice/vite.config.ts
+++ apps/backoffice/vite.config.ts
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    //If we have a problem with the build, we will need to force include the dependencies from the monorepo
+    //Keep it here so I remember the syntax
+    // optimizeDeps : {
+    //     include: ['@big/tailwindwonfig', '@big/ui'],
+    // },
 });
