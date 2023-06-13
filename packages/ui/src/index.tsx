4a44e4d56dddab73a4890e3b3bc5c1828dc217f3
gheorghe <gheorghe@macbook.local>
2023-06-13 22:05:36 +0200

    refactor(ui): package to handle tailwind with shadcn

---
+++ packages/ui/src/index.tsx
@@ -0,0 +1,5 @@
+export const Bonjour = () => {
+    return (
+        <p className="text-3xl text-center text-blue-500">BONJOUR DU PACKAGE</p>
+    );
+};
---
+++ packages/ui/tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+const sharedConfig = require('@big/tailwind-config');
+module.exports = {
+    prefix  : 'ui-',
+    presets : [sharedConfig],
+    content : ['./src/**/*.{js,ts,jsx,tsx}'],
+};
