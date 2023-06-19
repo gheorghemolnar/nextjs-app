5fc144db58f3fb01177a2ab89215038fe9c7fc4f
gheorghe.molnar <molnargheorghe@gmail.com>
2023-06-19 09:42:31 +0200

    refactor(@backoofice: change nomatch page): change NoMatch page

--- apps/backoffice/src/pages/NoMatch.tsx
+++ apps/backoffice/src/pages/NoMatch.tsx
@@ -2,11 +2,10 @@ import { Link } from "react-router-dom";
 
 export default function NoMath() {
   return (
-    <div>
-      <h2>Page introuvable !</h2>
-      <p>
-        <Link to="/">Retour à l'accueil</Link>
-      </p>
+    <div className="container mx-auto h-1/2 flex flex-column grow justify-center items-center">
+      <h1 className="text-lg">Page introuvable</h1>
+      <br/><br/>
+      <Link to="/">Retour à l'accueil</Link>
     </div>
   );
 }
