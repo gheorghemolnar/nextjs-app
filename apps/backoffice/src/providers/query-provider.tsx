26b5b4e8a47b68f57ddd7e5c5e68bf8524e3368c
loumnar <molnargheorghe@yahoo.com>
2023-06-22 13:24:25 +0200

    update env vars

--- apps/backoffice/src/providers/query-provider.tsx
+++ apps/backoffice/src/providers/query-provider.tsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const displayReactQueryDevtools = () => {
-    if (import.meta.env.NODE_ENV === 'development') {
+    if (import.meta.env.VITE_ENV === 'development') {
         return <ReactQueryDevtools />;
     }
 };
