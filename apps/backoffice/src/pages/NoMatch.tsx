b59d1ee9d5503aac3c84682d5457342c592385e8
loumnar <molnargheorghe@yahoo.com>
2023-06-26 14:36:30 +0200

    refactor(update 404): update 404

--- apps/backoffice/src/pages/NoMatch.tsx
+++ apps/backoffice/src/pages/NoMatch.tsx
@@ -2,8 +2,11 @@ import { Link } from 'react-router-dom';
 
 export default function NoMath() {
     return (
-        <div className="container mx-auto h-1/2 flex flex-column grow justify-center items-center">
-            <h1 className="text-lg">Page introuvable</h1>
+        <div
+            //className="container mx-auto h-1/2 flex flex-column grow justify-center items-center">
+            className="container mx-4 h-1/2"
+        >
+            <h1 className="text-3xl">Page introuvable</h1>
             <br />
             <br />
             <Link to="/">Retour à l'accueil</Link>
